e3242ce5e2541184943c8244db072c58069d0d6a
Marianoetchart <mariano.etchart@gmail.com>
2019-04-20 08:10:01 +0100

    debugging

--- deep_rl/agent/DRQN_agent.py
+++ deep_rl/agent/DRQN_agent.py
@@ -31,6 +31,8 @@ class DRQNActor(BaseActor):
         next_state, reward, done, info = self._task.step([action])
         if self.config.eval_flickering: 
                 random_prob = torch.rand(0,1)
+                print(self.config.ob_prob)
+                print(random_prob)
                 if random_prob > self.config.ob_prob:
                     next_state = np.zeros_like(next_state)
         entry = [self._state[0], action, reward[0], next_state[0], int(done[0]), info]
